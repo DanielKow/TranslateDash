5ca6f8fdcf65cf1d51d8958e20f74a199da3b0be
Daniel Kowalski <danielk.biznes@gmail.com>
2023-12-03 18:36:09 +0100

    Initial commit

---
+++ app/src/main/java/me/daniel/translatedash/data/DataSource.kt
@@ -0,0 +1,10 @@
+package me.daniel.translatedash.data
+
+import me.daniel.translatedash.R
+
+object DataSource {
+    val modes = listOf(
+        R.string.ten_words,
+        R.string.endless
+    )
+}
---
+++ app/src/main/java/me/daniel/translatedash/ui/ChooseModeScreen.kt
@@ -0,0 +1,63 @@
+package me.daniel.translatedash.ui
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.daniel.translatedash.R
+import me.daniel.translatedash.data.DataSource
+import me.daniel.translatedash.ui.theme.TranslateDashTheme
+
+@Composable
+fun ChooseModeScreen(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp),
+            horizontalArrangement = Arrangement.Center) {
+            Text(text = stringResource(R.string.choose_mode))
+        }
+        Divider()
+        Row(modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically) {
+            Column (
+                verticalArrangement = Arrangement.spacedBy(25.dp)){
+                DataSource.modes.forEach {
+                        item -> ModeButton(item)
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun ModeButton(
+    @StringRes labelResourceId: Int
+) {
+    Button(modifier = Modifier.fillMaxWidth(0.75f),
+        onClick = { /*TODO*/ }) {
+        Text(stringResource(labelResourceId))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChooseModeScreenPreview() {
+    TranslateDashTheme {
+        ChooseModeScreen()
+    }
+}
